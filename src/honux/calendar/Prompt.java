0b937497071aba9218a724a4421428e532d8bd47
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-05-12 05:47:58 -0600

    update

--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -1,30 +1,59 @@
 package honux.calendar;
 
-import java.util.Scanner;
+
 import java.util.*;
 
 public class Prompt {
-
-	private final static String PROMPT = "cal> ";
+/**
+ * 
+ * @param week
+ * @return 0 ~ ( 0 = Sunday, 6 = Saturday)
+ */
+	public int parseDay(String week) {
+		if(week.equals("su")) return 0;
+	    else if(week.equals("mo")) return 1;
+	    else if(week.equals("tu")) return 2;
+	    else if(week.equals("we")) return 3;
+	    else if(week.equals("th")) return 4;
+	    else if(week.equals("fr")) return 5;
+	    else if(week.equals("sa")) return 6;
+	    else	return 0;
+	}
+	
+	
 
 	public void runPrompt() {
 
 		Scanner scanner = new Scanner(System.in);
-		
 		calendar1 cal1 = new calendar1();
-		int month = 1;
-
+		
+		int month = -1;
+		int year = -1;
+		int weekday = 0;
 		while (true) {
-			System.out.println("Enter the Month.");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if (month == -1) {
+			System.out.println("Enter the Year. exit : -1");
+			System.out.print("YEAR> ");
+			year = scanner.nextInt();
+			if(year == -1)
 				break;
-			}
-			if (month >= 12) {
+
+			System.out.println("Enter the Month.");
+			System.out.print("MONTH> ");
+			month = scanner.nextInt();
+			
+			
+			System.out.println("First day ");
+			String str_weekday = scanner.next();
+			weekday = parseDay(str_weekday);
+			
+			
+			
+			
+			if (month > 12 || month < 1) {
+				System.out.println("Error");
 				continue;
 			}
-			cal1.printCalendar(2022, month);
+			cal1.printCalendar(year, month, weekday);
 		}
 		System.out.println("Bye~");
 		scanner.close();
--- src/honux/calendar/calendar1.java
+++ src/honux/calendar/calendar1.java
@@ -1,29 +1,58 @@
 package honux.calendar;
 
 public class calendar1 {
-	
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
-	public int getMaxDaysOfMonth(int month) {
-	 return MAX_DAYS[month - 1];	
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private static final int[] Leap_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 != 0))
+			return true;
+		else
+			return false;
 	}
 
-	
-	public void printCalendar(int year,int month) {
-		
-		System.out.printf("   <<%4d %3d>> \n",year,month);
-		System.out.println("Sun Mon Tue Wed Thur Fri Sat");
+	public int getMaxDaysOfMonth(int year, int month) {
+		if (isLeapYear(year)) {
+			return Leap_MAX_DAYS[month - 1];
+		} else {
+
+			return MAX_DAYS[month - 1];
+		}
+	}
+
+	public void printCalendar(int year, int month, int weekday) {
+
+		System.out.printf("   <<%4d %3d>> \n", year, month);
+		System.out.println("Su Mo Tu We Th Fr Sa");
 		System.out.println("-----------------------------");
-		
-		int maxDay = getMaxDaysOfMonth(month);
-		
-		for(int i = 1; i<=maxDay; i++) {
-			System.out.printf("%3d",i);
-			if(i % 7 ==0) {
+
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
+
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = (count <7) ? count : 0;
+
+		// print first line
+
+		for (int i = 1; i <= count; i++) {
+			System.out.printf("%3d", i);
+		}
+		System.out.println();
+
+		// print from second line to last line
+
+		count++;
+
+		for (int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if (i % 7 == delim) {
 				System.out.println();
 			}
 		}
 		System.out.println();
 	}
-	
 }
