240456ae77e18bef6c860fd8b2265194077661af
developer-Park <83831110+developer-Park@users.noreply.github.com>
2022-05-11 22:11:29 -0600

    init

---
+++ src/honux/calendar/Sum.java
@@ -0,0 +1,18 @@
+package honux.calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		System.out.println("Which number do you want add?");
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		String[] splitedValue = input.split(" ");
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		
+		System.out.println("Two number's sum result  =" + (first + second));
+	}
+
+}
